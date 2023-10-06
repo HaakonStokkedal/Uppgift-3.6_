94a3c8fec5afc7c773d1bb2ad3538e42647575e0
hast06001 <hast06001@E-PW024NW1>
2023-10-06 11:11:37 +0200

    Add project files.

---
+++ Uppgift 3.6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Upgift_3._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ditt för och efternamn.");
+            string helaNamn = Console.ReadLine();
+            int mellanslag = helaNamn.IndexOf(" ");
+
+            string förnamn = helaNamn[..mellanslag];
+            string efternamn = helaNamn[(mellanslag + 1)..];
+
+            if (förnamn.CompareTo(efternamn) < 0)
+                Console.WriteLine("Förnamnet kommet först i bokstavsordning");
+            else
+                Console.WriteLine("Efternamnet kommer först i bokstavsordning");
+
+            Console.ReadKey();
+        }
+    }
+}
